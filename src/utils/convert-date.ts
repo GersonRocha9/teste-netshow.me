644a474c66876bf07ba1f2a9073bef32def807f3
Gerson Rocha <gersonrocha9@gmail.com>
2024-10-09 20:13:49 -0300

    fix: edit convert date function

--- src/utils/convert-date.ts
+++ src/utils/convert-date.ts
@@ -1,10 +1,13 @@
-export function convertDate(date: string) {
-  const dateObject = new Date(date)
-  const day = dateObject.getDate()
-  const month = dateObject.getMonth() + 1
+export function convertDate(dateString: string): string {
+  const dateObject = new Date(dateString)
+
+  const formatNumber = (num: number): string => num.toString().padStart(2, '0')
+
+  const day = formatNumber(dateObject.getDate())
+  const month = formatNumber(dateObject.getMonth() + 1)
   const year = dateObject.getFullYear()
-  const hours = dateObject.getHours()
-  const minutes = dateObject.getMinutes()
+  const hours = formatNumber(dateObject.getHours())
+  const minutes = formatNumber(dateObject.getMinutes())
 
   return `${day}/${month}/${year} às ${hours}:${minutes}`
 }
