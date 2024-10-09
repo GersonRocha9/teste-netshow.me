042261c61946e0bc84f708326f92a3dd97b62bc7
Gerson Rocha <gersonrocha9@gmail.com>
2024-10-09 19:57:46 -0300

    fix: adjust padding top on android

--- src/screens/video-details/styles.ts
+++ src/screens/video-details/styles.ts
@@ -1,8 +1,9 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
 
 export const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
   container: {
     flex: 1,
