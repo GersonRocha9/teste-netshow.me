4d146e10a338b3080d56194b69f1bd49ded9d37d
Gerson Rocha <gersonrocha9@gmail.com>
2024-10-10 20:14:57 -0300

    refactor: change api url to works on android

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import { Platform } from 'react-native'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    Platform.OS === 'ios' ? 'http://localhost:3333' : 'http://10.0.2.2:3333',
 })
