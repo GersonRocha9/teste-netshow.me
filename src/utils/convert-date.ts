64454745e1c0ad765cd5aeb02b4c2945255557ed
Gerson Rocha <gersonrocha9@gmail.com>
2024-10-10 11:04:58 -0300

    fix: edit convert date function to use UTC date format

--- src/utils/convert-date.ts
+++ src/utils/convert-date.ts
@@ -3,11 +3,11 @@ export function convertDate(dateString: string): string {
 
   const formatNumber = (num: number): string => num.toString().padStart(2, '0')
 
-  const day = formatNumber(dateObject.getDate())
-  const month = formatNumber(dateObject.getMonth() + 1)
-  const year = dateObject.getFullYear()
-  const hours = formatNumber(dateObject.getHours())
-  const minutes = formatNumber(dateObject.getMinutes())
+  const day = formatNumber(dateObject.getUTCDate())
+  const month = formatNumber(dateObject.getUTCMonth() + 1)
+  const year = dateObject.getUTCFullYear()
+  const hours = formatNumber(dateObject.getUTCHours())
+  const minutes = formatNumber(dateObject.getUTCMinutes())
 
   return `${day}/${month}/${year} às ${hours}:${minutes}`
 }
